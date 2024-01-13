5daf7f4991a627f0e730d5c6224beca70192d3d5
Francisco Paxtian <francisco.izqpax@gmail.com>
2024-01-12 23:37:56 -0600

    install router success

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const Home = () => {
+    return <>
+      <h1>Este es el home</h1>
+    </>
+  }
+
+  const Calculadora = () => {
+    return <>
+      <h1>Este es una calculadora</h1>
+    </>
+  }
+  
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <>      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculadora" element={<Calculadora />} />
+        </Routes>
+      </Router>
     </>
   )
 }
